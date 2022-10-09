16dc83b9279a1b87f8beaa2d765a7b8ea7706f08
Alex <al.repin.a@yandex.ru>
2022-10-09 14:41:02 +0300

    I have changed 2

--- Task01_02.py
+++ Task01_02.py
@@ -2,9 +2,9 @@
 #¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 #not(x or y or z) = not x and not y and not z
 
-for x in range(1):
-    for y in range(1):
-        for z in range(1):
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
             if not(x or y or z) == (not x and not y and not z):
                 print(True)
             else:
